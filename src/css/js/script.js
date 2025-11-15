00dc9150f3a84ca284e8726e8a800cacb0ba67c6
Vinicius Vieira da Costa <131974514+vinioff@users.noreply.github.com>
2025-11-14 21:28:32 -0300

    Navbar Concluida
    Está comentado praticamente cada verso, em caso de fazer uma nova page pegue o css, html e o js da navbar que está feito

---
+++ src/css/js/script.js
@@ -0,0 +1,11 @@
+// "DOMContentLoaded" espera o HTML ser carregado.
+document.addEventListener('DOMContentLoaded', (event) => {
+    
+    // O código da navbar (openBtn) foi removido
+    // porque ela agora funciona 100% com CSS (:hover).
+
+    // Você pode adicionar aqui futuros códigos JS,
+    // como funções para o mapa ou para o chatbot.
+    console.log("Página carregada e pronta!");
+
+});
